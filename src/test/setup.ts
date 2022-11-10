76250b06cb17eee02c254bec65ed9b237f6ddc1c
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-11-10 04:27:54 +0100

    feat: add global config for storybook react testing

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom";
+import { setGlobalConfig } from "@storybook/testing-react";
+import * as globalStorybookConfig from "@storybookConfig/preview";
+
+setGlobalConfig(globalStorybookConfig);
